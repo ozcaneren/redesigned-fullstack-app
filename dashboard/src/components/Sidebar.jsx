5e9eda6b2c72e5dd78b69dc00fcae84b27c73ddf
eren ozcan <erenozcandev@gmail.com>
2023-09-29 15:49:33 +0300

    add dropdown menu on sidebar

--- dashboard/src/components/Sidebar.jsx
+++ dashboard/src/components/Sidebar.jsx
@@ -1,6 +1,13 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Sidebar() {
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!isDropdownOpen);
+  };
+
   return (
     <div>
       <div className="fixed flex flex-col top-14 left-0 w-14 hover:w-64 md:w-64 bg-zinc-800 h-full text-gray-200 font-medium transition-all duration-300 z-10">
@@ -63,9 +70,9 @@ function Sidebar() {
                 </div>
               </div>
             </li>
-            <li>
-              <Link
-                to="/room"
+            <li className="">
+              <span
+                onClick={toggleDropdown}
                 className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-zinc-700 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-gray-800 pr-6"
               >
                 <span className="inline-flex justify-center items-center ml-4">
@@ -86,7 +93,70 @@ function Sidebar() {
                 <span className="ml-2 text-md tracking-wide truncate">
                   Oda Yönetimi
                 </span>
-              </Link>
+                <span
+                  className={`ml-2 ${
+                    isDropdownOpen ? "rotate-180" : "rotate-0"
+                  } transition-transform`}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-4 h-4"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M6 9l6 6 6-6" />
+                  </svg>
+                </span>
+              </span>
+              {isDropdownOpen && (
+                <ul className="mt-2 space-y-2">
+                  <li className="ml-2 hover:bg-zinc-700 cursor-pointer">
+                    <Link to="/room">
+                      <span className="inline-flex justify-center items-center ml-4">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="w-5 h-5"
+                          fill="none"
+                          stroke="currentColor"
+                          viewBox="0 0 24 24"
+                        >
+                          <path d="M9 6 6.5 3.5a1.5 1.5 0 0 0-1-.5C4.683 3 4 3.683 4 4.5V17a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-5" />
+                          <line x1="10" x2="8" y1="5" y2="7" />
+                          <line x1="2" x2="22" y1="12" y2="12" />
+                          <line x1="7" x2="7" y1="19" y2="21" />
+                          <line x1="17" x2="17" y1="19" y2="21" />
+                        </svg>
+                      </span>
+                      <span className="ml-2 text-md tracking-wide truncate">
+                        Odalarimiz
+                      </span>
+                    </Link>
+                  </li>
+                  <li className="ml-2 hover:bg-zinc-700 cursor-pointer">
+                    <Link to="/room/feature">
+                      <span className="inline-flex justify-center items-center ml-4">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="w-5 h-5"
+                          fill="none"
+                          stroke="currentColor"
+                          viewBox="0 0 24 24"
+                        >
+                          <path d="M9 6 6.5 3.5a1.5 1.5 0 0 0-1-.5C4.683 3 4 3.683 4 4.5V17a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-5" />
+                          <line x1="10" x2="8" y1="5" y2="7" />
+                          <line x1="2" x2="22" y1="12" y2="12" />
+                          <line x1="7" x2="7" y1="19" y2="21" />
+                          <line x1="17" x2="17" y1="19" y2="21" />
+                        </svg>
+                      </span>
+                      <span className="ml-2 text-md tracking-wide truncate">
+                        Oda Özellik Yönetimi
+                      </span>
+                    </Link>
+                  </li>
+                </ul>
+              )}
             </li>
           </ul>
           <p className="mb-14 px-5 py-3 hidden md:block text-center text-xs">
