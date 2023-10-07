84e81b41166dbeef3eb39b5a22a2a99cd3c8bece
Eren Ozcan <erenozcandev@gmail.com>
2023-10-08 02:06:55 +0300

    featureAdd page add more style

--- dashboard/src/pages/Feature.jsx
+++ dashboard/src/pages/Feature.jsx
@@ -101,15 +101,15 @@ const Feature = () => {
                     </div>
                   </div>
                 </div>
-                <div className="flex justify-center items-center flex-col space-y-4 py-4">
+                <div className="flex justify-between py-4 px-4">
                   {/* Türkçe Özellikler */}
-                  <div className="w-6/12 px-4 py-4 relative rounded-[6px] overflow-x-auto border border-solid border-zinc-700">
-                    <h2 className="text-lg font-semibold mb-4">
+                  <div className="w-[760px] py-4 relative rounded-[6px] overflow-x-auto border border-solid border-zinc-700">
+                    <h2 className="text-lg text-center font-semibold mb-4">
                       Türkçe Özellikler
                     </h2>
                     <table className="text-sm w-full text-left text-[#202020]">
                       <thead className="text-xs uppercase bg-zinc-700 text-gray-200">
-                        <tr className="">
+                        <tr className="flex justify-between">
                           <th scope="col" className="px-6 py-3">
                             Özellik
                           </th>
@@ -121,13 +121,13 @@ const Feature = () => {
                       <tbody>
                         {turkishFeatures.map((feature, index) => (
                           <tr
-                            className="border-b-1 border-gray-300 text-white bg-zinc-800 hover:bg-zinc-800"
+                            className="border-b-1 flex justify-between border-gray-300 text-white bg-zinc-800 hover:bg-zinc-800"
                             key={index}
                           >
-                            <td className="px-6 py-4">
+                            <td className="py-4 px-6">
                               {feature.TurkishFeature}
                             </td>
-                            <td className="px-10 py-4">
+                            <td className="py-4 px-12">
                               <button
                                 className=""
                                 onClick={() => handleTurkishDelete(feature._id)}
@@ -141,13 +141,13 @@ const Feature = () => {
                     </table>
                   </div>
                   {/* İngilizce Özellikler */}
-                  <div className="w-6/12 px-4 py-4 relative rounded-[6px] overflow-x-auto border border-solid border-zinc-700">
-                    <h2 className="text-lg font-semibold mb-4">
+                  <div className="w-[760px] py-4 relative rounded-[6px] overflow-x-auto border border-solid border-zinc-700">
+                    <h2 className="text-lg text-center font-semibold mb-4">
                       İngilizce Özellikler
                     </h2>
                     <table className="text-sm w-full text-left text-[#202020]">
                       <thead className="text-xs uppercase bg-zinc-700 text-gray-200">
-                        <tr>
+                        <tr className="flex justify-between">
                           <th scope="col" className="px-6 py-3">
                             Özellik
                           </th>
@@ -159,13 +159,13 @@ const Feature = () => {
                       <tbody>
                         {englishFeatures.map((feature, index) => (
                           <tr
-                            className="border-b-1 border-gray-300 text-white bg-zinc-800 hover:bg-zinc-800"
+                            className="border-b-1 flex justify-between border-gray-300 text-white bg-zinc-800 hover:bg-zinc-800"
                             key={index}
                           >
                             <td className="px-6 py-4">
                               {feature.EnglishFeature}
                             </td>
-                            <td className="px-10 py-4">
+                            <td className="px-12 py-4">
                               <button
                                 className=""
                                 onClick={() => handleEnglishDelete(feature._id)}
--- dashboard/src/pages/FeatureAdd.jsx
+++ dashboard/src/pages/FeatureAdd.jsx
@@ -26,7 +26,10 @@ const FeatureAdd = () => {
         TurkishFeature: language === "Turkish" ? TurkishFeature : "",
         EnglishFeature: language === "English" ? EnglishFeature : "",
       };
-      await axios.post(`http://localhost:4000/api/extrafeature/${language.toLowerCase()}`, newExtraFeature);
+      await axios.post(
+        `http://localhost:4000/api/extrafeature/${language.toLowerCase()}`,
+        newExtraFeature
+      );
       navigate(`/room/feature`);
       setTurkishFeature("");
       setEnglishFeature("");
@@ -50,8 +53,11 @@ const FeatureAdd = () => {
             <div className="flex justify-center items-center">
               <div className="w-full flex justify-center rounded-sm bg-zinc-800">
                 <div className="w-full relative rounded-[6px] p-4 overflow-x-auto">
-                  <div className="mx-auto mt-4">
-                    <form onSubmit={(e) => handleSubmit(e, "Turkish")}>
+                  <div className="mx-auto mt-4 space-y-4">
+                    <form
+                      className=""
+                      onSubmit={(e) => handleSubmit(e, "Turkish")}
+                    >
                       <div className="mb-4">
                         <label
                           htmlFor="TurkishFeature"
@@ -61,19 +67,29 @@ const FeatureAdd = () => {
                         </label>
                         <input
                           type="text"
-                          className="mt-1 p-2 w-full text-black bg-white border-gray-300 border rounded-md"
+                          className="mt-1 p-2 text-black bg-white border-gray-300 border rounded-md"
                           id="TurkishFeature"
                           placeholder="Türkçe Özellik"
                           value={TurkishFeature}
                           onChange={(e) => setTurkishFeature(e.target.value)}
                         />
                       </div>
-                      <button type="submit" className="btn btn-primary">
-                        Türkçe Ekle
+                      <div className="">
+                      <button
+                        type="submit"
+                        className="border border-gray-200 p-1 bg-zinc-700"
+                      >
+                        Türkçe Olarak Ekle
                       </button>
+                      </div>
                     </form>
-                    <div className="h-[1px] w-full bg-gray-500 my-4"></div>
-                    <form onSubmit={(e) => handleSubmit(e, "English")}>
+                    <div className="bg-gray-300 w-full h-[1px]"></div>
+                    <div className="bg-gray-300 w-full h-[1px]"></div>
+                    <div className="bg-gray-300 w-full h-[1px]"></div>
+                    <form
+                      className=""
+                      onSubmit={(e) => handleSubmit(e, "English")}
+                    >
                       <div className="mb-4">
                         <label
                           htmlFor="EnglishFeature"
@@ -83,16 +99,21 @@ const FeatureAdd = () => {
                         </label>
                         <input
                           type="text"
-                          className="mt-1 p-2 w-full text-black bg-white border-gray-300 border rounded-md"
+                          className="mt-1 p-2 text-black bg-white border-gray-300 border rounded-md"
                           id="EnglishFeature"
                           placeholder="İngilizce Özellik"
                           value={EnglishFeature}
                           onChange={(e) => setEnglishFeature(e.target.value)}
                         />
                       </div>
-                      <button type="submit" className="btn btn-primary">
-                        İngilizce Ekle
-                      </button>
+                      <div className="">
+                        <button
+                          type="submit"
+                          className=" border border-gray-200 p-1 bg-zinc-700"
+                        >
+                          İngilizce Olarak Ekle
+                        </button>
+                      </div>
                     </form>
                   </div>
                 </div>
